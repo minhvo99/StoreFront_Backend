4d8f960ceb5fec9784266877ecf7cbe42f196230
Minh Vo <minhvo2607dtu@gmail.com>
2023-07-07 14:06:44 +0700

    connect db

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var app = (0, express_1.default)();
+var port = 3000;
+app.use((0, cors_1.default)());
+app.get('/', function (req, res) {
+    res.send('Storefront Backend project Udacity');
+});
+app.listen(port, function () {
+    console.log("Server is running on port http://localhost:".concat(port));
+});
